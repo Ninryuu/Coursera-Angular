6ea2b9f13ccea55d0003b65eda4a9fed7d222a36
Damian <damiansz.88@gmail.com>
2018-11-20 09:27:16 -0300

    RxJs Part 1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -13,7 +13,7 @@ export class AboutComponent implements OnInit {
 
   constructor(private leaderService : LeaderService) { 
     this.leaderService.getLeaders()
-    .then(leaders => this.leaders = leaders);
+    .subscribe(leaders => this.leaders = leaders);
   }
 
   ngOnInit() {
